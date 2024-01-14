a0166a2b9048d9556d60606711127406ce1482b9
MayKitsune <kitsunecreativecode@gmail.com>
2024-01-14 01:09:34 -0300

    funcion para obtener data de API drink y agregarla al objeto del user

--- api_utils.py
+++ api_utils.py
@@ -8,3 +8,23 @@ def get_random_users(limit=10, categorize=None):
         data = sorted(data, key=lambda x: x['gender'])
         
         return data
+    
+def get_user_with_favorite_drink():
+    response_users = requests.get('https://randomuser.me/api/?results=10')
+    users_data = response_users.json()['results']
+    
+    response_drinks = requests.get('https://thecocktaildb.com/api/json/v1/1/random.php')
+    drink_data = response_drinks.json()['drinks'][0]
+    
+    result = []
+    for user in users_data:
+        user_with_drink = {
+            'name': user['name']['first'],
+            'last_name': user['name']['last'],
+            'email': user['email'],
+            'nationality': user['nat'],
+            'favourite_drink': drink_data['strDrink']
+        }
+        result.append(user_with_drink)
+        
+    return result
