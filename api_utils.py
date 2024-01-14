105cb3756636e88cd88c8778a53dc14f3f2b7485
MayKitsune <kitsunecreativecode@gmail.com>
2024-01-14 01:52:31 -0300

    correciones

--- api_utils.py
+++ api_utils.py
@@ -1,3 +1,4 @@
+# api_utils.py
 import requests
 
 def get_random_users(limit=10, categorize=None):
@@ -14,16 +15,16 @@ def get_user_with_favorite_drink():
     users_data = response_users.json()['results']
 
     response_drinks = requests.get('https://www.thecocktaildb.com/api/json/v1/1/random.php')
-    drink_data = response_drinks.json()['drinks']
+    drink_data = response_drinks.json()['drinks'][0]
 
     result = []
-    for user, drink in zip(users_data, drink_data):
+    for user in users_data:
         user_with_drink = {
             'name': user['name']['first'],
             'last_name': user['name']['last'],
             'email': user['email'],
             'nationality': user['nat'],
-            'favorite_drink': drink['strDrink']
+            'favorite_drink': drink_data['strDrink']
         }
         result.append(user_with_drink)
 
--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+# app.py
 from flask import Flask, jsonify, request
 from api_utils import get_random_users, get_user_with_favorite_drink
 
 app = Flask(__name__)
 
-@app.route('/users', methods=['Get'])
+@app.route('/users', methods=['GET'])
 def get_users():
     limit = int(request.args.get('limit', 10))
     categorize = request.args.get('categorize')
@@ -11,11 +12,10 @@ def get_users():
     data = get_random_users(limit, categorize)
     return jsonify(data)
 
-@app.route('/users', methods=['Get'])
-def get_users_with_favorite_drink():
-    data = get_user_with_favorite_drink
+@app.route('/users/drink', methods=['GET'])
+def get_users_with_drink():
+    data = get_user_with_favorite_drink()
     return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
-
