5ba7300a56f8ccf6ab1761906f4ccc0f593e1056
MayKitsune <kitsunecreativecode@gmail.com>
2024-01-14 01:47:29 -0300

    correcciones

--- api_utils.py
+++ api_utils.py
@@ -1,30 +1,30 @@
 import requests
 
 def get_random_users(limit=10, categorize=None):
-    response = requests.get(f'https://randomusers.me/api/?results={limit}')
+    response = requests.get(f'https://randomuser.me/api/?results={limit}')
     data = response.json()['results']
-    
+
     if categorize == 'gender':
         data = sorted(data, key=lambda x: x['gender'])
-        
-        return data
-    
+
+    return data
+
 def get_user_with_favorite_drink():
     response_users = requests.get('https://randomuser.me/api/?results=10')
     users_data = response_users.json()['results']
-    
-    response_drinks = requests.get('https://thecocktaildb.com/api/json/v1/1/random.php')
-    drink_data = response_drinks.json()['drinks'][0]
-    
+
+    response_drinks = requests.get('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+    drink_data = response_drinks.json()['drinks']
+
     result = []
-    for user in users_data:
+    for user, drink in zip(users_data, drink_data):
         user_with_drink = {
             'name': user['name']['first'],
             'last_name': user['name']['last'],
             'email': user['email'],
             'nationality': user['nat'],
-            'favourite_drink': drink_data['strDrink']
+            'favorite_drink': drink['strDrink']
         }
         result.append(user_with_drink)
-        
+
     return result
